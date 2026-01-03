dd8a09fd9df4765e2ccfbfc593d38e7292d13ce8
konrad-iwan <98711242+konrad-iwan@users.noreply.github.com>
2026-01-03 21:34:15 +0100

    add migration - seed db diary entries

--- DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -11,5 +11,34 @@ namespace DiaryApp.Data
         }
 
         public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                new DiaryEntry
+                {
+                    Id = 1,
+                    Title = "Went Hiking",
+                    Content = "Went hiking with Joe!",
+                    Created = new DateTime(2020, 1, 1)
+                },
+                new DiaryEntry
+                {
+                    Id = 2,
+                    Title = "Went Shopping",
+                    Content = "Went shopping with Joe!",
+                    Created = new DateTime(2020, 1, 2)
+                },
+                 new DiaryEntry
+                 {
+                     Id = 3,
+                     Title = "Went Diving",
+                     Content = "Went diving with Joe!",
+                     Created = new DateTime(2020, 1, 3)
+                 }
+                );                
+        }
     }
 }
